a39195b2d46520b7bd947dffec7aa704fabd9734
Yaman Umuroglu <maltanar@gmail.com>
2024-11-22 21:58:34 +0100

    copy over AXI-MM utils from pyverilator

--- py/pyxsi_utils.py
+++ py/pyxsi_utils.py
@@ -34,6 +34,73 @@ import subprocess
 import sys
 from typing import Optional
 
+axilite_expected_signals = [
+    "AWVALID",
+    "AWREADY",
+    "AWADDR",
+    "WVALID",
+    "WREADY",
+    "WDATA",
+    "WSTRB",
+    "ARVALID",
+    "ARREADY",
+    "ARADDR",
+    "RVALID",
+    "RREADY",
+    "RDATA",
+    "RRESP",
+    "BVALID",
+    "BREADY",
+    "BRESP",
+]
+
+aximm_expected_signals = [
+    "AWVALID",
+    "AWREADY",
+    "AWADDR",
+    "AWID",
+    "AWLEN",
+    "AWSIZE",
+    "AWBURST",
+    "AWLOCK",
+    "AWCACHE",
+    "AWPROT",
+    "AWQOS",
+    "AWREGION",
+    "AWUSER",
+    "WVALID",
+    "WREADY",
+    "WDATA",
+    "WSTRB",
+    "WLAST",
+    "WID",
+    "WUSER",
+    "ARVALID",
+    "ARREADY",
+    "ARADDR",
+    "ARID",
+    "ARLEN",
+    "ARSIZE",
+    "ARBURST",
+    "ARLOCK",
+    "ARCACHE",
+    "ARPROT",
+    "ARQOS",
+    "ARREGION",
+    "ARUSER",
+    "RVALID",
+    "RREADY",
+    "RDATA",
+    "RLAST",
+    "RID",
+    "RUSER",
+    "RRESP",
+    "BVALID",
+    "BREADY",
+    "BRESP",
+    "BID",
+    "BUSER",
+]
 
 def launch_process_helper(args, proc_env=None, cwd=None):
     """Helper function to launch a process in a way that facilitates logging
@@ -309,3 +376,108 @@ def rtlsim_multi_io(
             )
 
     return total_cycle_count
+
+def wait_for_handshake(sim, ifname, basename="s_axi_control_", dataname="DATA"):
+    """Wait for handshake (READY and VALID high at the same time) on given
+    interface on pyxsi sim object.
+
+    Arguments:
+    - sim : pyxsi sim object
+    - ifname : name for decoupled interface to wait for handshake on
+    - basename : prefix for decoupled interface name
+    - dataname : interface data sig name, will be return value if it exists
+
+    Returns: value of interface data signal during handshake (if given by dataname),
+    None otherwise (e.g. if there is no data signal associated with interface)
+    """
+    ret = None
+    while 1:
+        hs = (
+            _read_signal(sim, basename + ifname + "READY") == 1
+            and _read_signal(sim, basename + ifname + "VALID") == 1
+        )
+        try:
+            ret = _read_signal(sim, basename + ifname + dataname)
+        except Exception:
+            ret = None
+        toggle_clk(sim)
+        if hs:
+            break
+    return ret
+
+
+def multi_handshake(sim, ifnames, basename="s_axi_control_"):
+    """Perform a handshake on list of interfaces given by ifnames. Will assert
+    VALID and de-assert after READY observed, in as few cycles as possible."""
+
+    done = []
+    for ifname in ifnames:
+        _write_signal(sim, basename + ifname + "VALID", 1)
+    while len(ifnames) > 0:
+        for ifname in ifnames:
+            if (
+                _read_signal(sim, basename + ifname + "READY") == 1
+                and _read_signal(sim, basename + ifname + "VALID") == 1
+            ):
+                done.append(ifname)
+        toggle_clk(sim)
+        for ifname in done:
+            if ifname in ifnames:
+                ifnames.remove(ifname)
+            _write_signal(sim, basename + ifname + "VALID", 0)
+
+
+def axilite_write(sim, addr, val, basename="s_axi_control_", wstrb=0xF, sim_addr_and_data=True):
+    """Write val to addr on AXI lite interface given by basename.
+
+    Arguments:
+    - sim : PyVerilator sim object
+    - addr : address for write
+    - val : value to be written at addr
+    - basename : prefix for AXI lite interface name
+    - wstrb : write strobe value to do partial writes, see AXI protocol reference
+    - sim_addr_and_data : handshake AW and W channels simultaneously
+    """
+    _write_signal(sim, basename + "WSTRB", wstrb)
+    _write_signal(sim, basename + "WDATA", val)
+    _write_signal(sim, basename + "AWADDR", addr)
+    if sim_addr_and_data:
+        multi_handshake(sim, ["AW", "W"], basename=basename)
+    else:
+        _write_signal(sim, basename + "AWVALID", 1)
+        wait_for_handshake(sim, "AW", basename=basename)
+        # write request done
+        _write_signal(sim, basename + "AWVALID", 0)
+        # write data
+        _write_signal(sim, basename + "WVALID", 1)
+        wait_for_handshake(sim, "W", basename=basename)
+        # write data OK
+        _write_signal(sim, basename + "WVALID", 0)
+    # wait for write response
+    _write_signal(sim, basename + "BREADY", 1)
+    wait_for_handshake(sim, "B", basename=basename)
+    # write response OK
+    _write_signal(sim, basename + "BREADY", 0)
+
+
+def axilite_read(sim, addr, basename="s_axi_control_"):
+    """Read val from addr on AXI lite interface given by basename.
+
+    Arguments:
+    - sim : PyVerilator sim object
+    - addr : address for read
+    - basename : prefix for AXI lite interface name
+
+    Returns: read value from AXI lite interface at given addr
+    """
+    _write_signal(sim, basename + "ARADDR", addr)
+    _write_signal(sim, basename + "ARVALID", 1)
+    wait_for_handshake(sim, "AR", basename=basename)
+    # read request OK
+    _write_signal(sim, basename + "ARVALID", 0)
+    # wait for read response
+    _write_signal(sim, basename + "RREADY", 1)
+    ret_data = wait_for_handshake(sim, "R", basename=basename)
+    _write_signal(sim, basename + "RREADY", 0)
+    return ret_data
+
